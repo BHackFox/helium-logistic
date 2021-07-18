d2ea58389ba266218f1c8fc93637cfb46014030e
BHackFox <frosatelli@gmail.com>
2021-07-18 18:08:55 +0200

    Add Group Object

--- data/collector.js
+++ data/collector.js
@@ -9,15 +9,70 @@ function collector(data){
     username:null,
     password:null,
     email:null,
+    Group:{
+      groupName:null,
+      groupID:null,
+      groupRole:null
+    },
     Settings:{
       blackTheme:null
     }
+
+  }
+
+  var device = {
+    deviceName:null,
+    deviceID:null,
+    groupID:null,
+    beacons:{
+      name:null,
+      timelast:null,
+      time:[
+        null,
+        null
+      ]
+    },
+    data:[
+      {
+        dataID:null,
+        time:null,
+        status:null,
+        long:null,
+        lat:null,
+        stat:null
+      }
+    ]
+  }
+  // I device hanno un nome (univoco) e sono associati a un ID group (univoco)
+  // Solamente i group a cui appartengono i device possono vedere le informazioni in questo modo
+  // Bisogna creare un ID group univoco e un ID device univoco
+
+  var group = {
+    groupName:null,
+    groupID:null,
+    groupCreator:{
+      userName:null,
+      userID:null,
+      time:null
+    },
+    members:[
+      {
+        memberName:null,
+        memberID:null,
+        memberRole:null
+      }
+    ],
     Devices:[
       {
-        name:null,
-        location:null,
-        status:null,
-        lastID:null,
+        deviceName:null,
+        deviceID:null,
+        lastData:{
+          time:null,
+          status:null,
+          long:null,
+          lat:null,
+          stat:null
+        },
         connections:[
           {
             beacon:null,
@@ -32,27 +87,13 @@ function collector(data){
     ]
   }
 
-  var device_data = {
-    deviceName:null,
-    userID:null,
-    beacon:{
-      name:null,
-      timelast:null,
-      time:[
-        null,
-        null
-      ]
-    },
-    data:[
-      {
-        id:null,
-        time:null,
-        status:null,
-        long:null,
-        lat:null,
-        stat:null
-      }
-    ]
-  }
+  // L'oggetto group è molto articolato
+  // Quello che fa è aggregare i devices e permettere ai membri del gruppo di visualizzarli
+  // Ogni membro ha un ruolo
+  // Creator: Creatore e gestore del gruppo e dei devices -> sottogruppi: tutti
+  // Admin: Gestore del gruppo e dei devices -> sottogruppi: tutti tranne Creator
+  // Editor: Gestore dei devices -> sottogruppi: user
+  // User: Visualizzatore dei device -> sottogruppi: nessuno
+
   
 }
